0e998c8088ecd539ceb88ab8a724938047810512
www <www@www.com>
2019-05-04 11:15:16 +0800

    x

--- upool.c
+++ upool.c
@@ -132,6 +132,7 @@ static u32 upoolAddData(Upool* pool, const void* elmData, u32 elmSize)
 {
     u32 offset = pool->dataBuf.length;
     vec_pusharr(&pool->dataBuf, elmData, elmSize);
+    vec_push(&pool->dataBuf, 0);
     u32 a = align(pool->dataBuf.length, sizeof(uintptr_t));
     vec_resize(&pool->dataBuf, a);
     return offset;
